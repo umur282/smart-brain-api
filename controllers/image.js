25dd73187a144ffa3a86e7ec1a11a32626e72f7d
umur282 <umur.282@gmail.com>
2020-06-13 10:38:15 +0200

    fix api-key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: '7492771a7a3c498c814a875488867cc1'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
